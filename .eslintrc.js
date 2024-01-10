c20e905450a94f26749dc1771658545be5e452b4
bomsbro <qorwogjs@gmail.com>
2024-01-10 20:41:56 +0900

    feat(env): configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,36 @@ module.exports = {
 		'plugin:import/errors',
 		'plugin:import/warnings',
 	],
-	plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'import'],
+	plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'import', 'eslint-plugin-unused-imports'],
+	parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+	settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: './tsconfig.json',
+      },
+    },
+  },
+	overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
 	rules: {
 		'prettier/prettier': ['error'],
 		'no-param-reassign': ['error', { props: false }],
 	},
+	ignorePatterns: ['build/**', 'dist/**', 'node_modules/', '/*.js'],
 };
