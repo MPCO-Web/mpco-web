89d54a54ae01be9199b2e8835c008b1dd5a56200
bomsbro <qorwogjs@gmail.com>
2024-01-10 22:10:38 +0900

    fix: env

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,20 @@ module.exports = {
 		es6: true,
 		jest: true,
 	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		sourceType: 'module',
+	},
+	plugins: [
+		'prettier',
+		'@typescript-eslint',
+		'react',
+		'react-hooks',
+		'jsx-a11y',
+		'import',
+		'eslint-plugin-unused-imports',
+	],
 	extends: [
 		'airbnb',
 		'airbnb-typescript',
@@ -17,36 +31,35 @@ module.exports = {
 		'plugin:import/errors',
 		'plugin:import/warnings',
 	],
-	plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'import', 'eslint-plugin-unused-imports'],
-	parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
 	settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-        project: './tsconfig.json',
-      },
-    },
-  },
+		react: {
+			version: 'detect',
+		},
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts', '.tsx'],
+		},
+		'import/resolver': {
+			typescript: {
+				alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+				project: './tsconfig.json',
+			},
+		},
+	},
 	overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        'no-undef': 'off',
-      },
-    },
-  ],
+		{
+			files: ['*.ts', '*.tsx'],
+			rules: {
+				'no-undef': 'off',
+			},
+		},
+	],
 	rules: {
 		'prettier/prettier': ['error'],
 		'no-param-reassign': ['error', { props: false }],
+		'react/function-component-definition': [
+			2,
+			{ namedComponents: ['arrow-function', 'function-declaration'] },
+		],
 	},
 	ignorePatterns: ['build/**', 'dist/**', 'node_modules/', '/*.js'],
 };
