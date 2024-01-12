c52e529d7462eb2344ffdb5be9e355d75ddeb9da
bomsbro <qorwogjs@gmail.com>
2024-01-12 22:13:51 +0900

    init: webpack alias

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,18 @@
+const path = require('path');
 const CracoAlias = require('craco-alias');
 
 module.exports = {
+	webpack: {
+		configure: (webpackConfig, { env, paths }) => {
+			webpackConfig.resolve = {
+				alias: {
+					react: path.resolve('./node_modules/react'),
+				},
+				extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+			};
+			return webpackConfig;
+		},
+	},
 	plugins: [
 		{
 			plugin: CracoAlias,
