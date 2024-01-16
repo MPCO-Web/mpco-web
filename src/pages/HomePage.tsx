d677f8550c0ad63385120a7c76a349154e95fab2
bomsbro <qorwogjs@gmail.com>
2024-01-16 19:16:27 +0900

    init: add mui

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { css } from '@emotion/css';
+import { Button } from '@mui/material';
 
 const HomePage = () => {
 	// tailwindcss classes
@@ -22,6 +23,9 @@ const HomePage = () => {
 			>
 				Hello mpco
 			</h1>
+			<Button variant="text">Text</Button>
+			<Button variant="contained">Contained</Button>
+			<Button variant="outlined">Outlined</Button>
 		</div>
 	);
 };
