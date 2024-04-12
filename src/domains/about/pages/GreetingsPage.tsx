ddfe53889e820b984a7b701509f856f8f021e425
dpwlsz <dpwls0875@konkuk.ac.kr>
2024-04-13 00:53:46 +0900

    feat(domain): greetings page

--- src/domains/about/pages/GreetingsPage.tsx
+++ src/domains/about/pages/GreetingsPage.tsx
@@ -8,6 +8,11 @@ const GreetingsPage = () => {
 			<Banner text="MPCO에 방문해 주셔서 감사합니다." />
 			<section className="w-full max-w-screen-xl mx-auto">
 				<h3>인사말</h3>
+				<p>
+					안녕하세요. MPCO는 미래의 세계를 위해 노력하는 기업입니다. 저희는 기술을 통해 사람들의
+					삶을 더 편리하고 행복하게 만들기 위해 노력하고 있습니다. 미래에는 더 많은 사람들이 MPCO를
+					통해 더 행복해지길 바랍니다.
+				</p>
 			</section>
 		</>
 	);
